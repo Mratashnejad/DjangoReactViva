616ff27b0f40ba9aafc9f00914f7fb29858b38de
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-07-30 09:59:06 +0400

    Using ViewSet

--- Backend/src/Extrashift/api/urls.py
+++ Backend/src/Extrashift/api/urls.py
@@ -1,17 +1,27 @@
-from django.urls import path, include
-from .views import (
-    ExtrashiftListView,
-    ExtrashiftCreateView,
-    ExtrashiftDetailView,
-    ExtrashiftUpdateView,
-    ExtrashiftDeleteView
-)
+from Extrashift.api.views import ExtrashiftViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', ExtrashiftViewSet, basename='Extrashift')
+urlpatterns = router.urls
 
 
-urlpatterns = [
-    path('', ExtrashiftListView.as_view()),
-    path('create/', ExtrashiftCreateView.as_view()),
-    path('<pk>', ExtrashiftDetailView.as_view()),
-    path('<pk>/update/', ExtrashiftUpdateView.as_view()),
-    path('<pk>/delete/', ExtrashiftDeleteView.as_view()),
-]
+# ------------------this is Old and To Many Code ---------------------
+
+# from django.urls import path, include
+# from .views import (
+#     ExtrashiftListView,
+#     ExtrashiftCreateView,
+#     ExtrashiftDetailView,
+#     ExtrashiftUpdateView,
+#     ExtrashiftDeleteView
+# )
+
+
+# urlpatterns = [
+#     path('', ExtrashiftListView.as_view()),
+#     path('create/', ExtrashiftCreateView.as_view()),
+#     path('<pk>', ExtrashiftDetailView.as_view()),
+#     path('<pk>/update/', ExtrashiftUpdateView.as_view()),
+#     path('<pk>/delete/', ExtrashiftDeleteView.as_view()),
+# ]
--- Backend/src/Extrashift/api/views.py
+++ Backend/src/Extrashift/api/views.py
@@ -1,35 +1,44 @@
+from rest_framework import viewsets
 from Extrashift.models import Extrashift
 from .Serializers import ExtrashiftSerializers
 
-from rest_framework.generics import (
-    ListAPIView,
-    RetrieveAPIView,
-    CreateAPIView,
-    UpdateAPIView,
-    DestroyAPIView
-)
 
-#its for List API Object View
-class ExtrashiftListView(ListAPIView):
-    queryset = Extrashift.objects.all()
+class ExtrashiftViewSet(viewsets.ModelViewSet):
     serializer_class = ExtrashiftSerializers
+    queryset = Extrashift.objects.all()
 
-#its for Detail of API Object View
-class ExtrashiftDetailView(RetrieveAPIView):
-    queryset = Extrashift.objects.all()
-    serializer_class = ExtrashiftSerializers
 
-#its for Create New Object View
-class ExtrashiftCreateView(CreateAPIView):
-    queryset = Extrashift.objects.all()
-    serializer_class = ExtrashiftSerializers
+# ------------------this is Old and To Many Code ---------------------
 
-#its for Updateing Object View After the object got some Change
-class ExtrashiftUpdateView(UpdateAPIView):
-    queryset = Extrashift.objects.all()
-    serializer_class = ExtrashiftSerializers
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView
+# )
 
-#its for Deleting Object View
-class ExtrashiftDeleteView(DestroyAPIView):
-    queryset = Extrashift.objects.all()
-    serializer_class = ExtrashiftSerializers
+# #its for List API Object View
+# class ExtrashiftListView(ListAPIView):
+#     queryset = Extrashift.objects.all()
+#     serializer_class = ExtrashiftSerializers
+
+# #its for Detail of API Object View
+# class ExtrashiftDetailView(RetrieveAPIView):
+#     queryset = Extrashift.objects.all()
+#     serializer_class = ExtrashiftSerializers
+
+# #its for Create New Object View
+# class ExtrashiftCreateView(CreateAPIView):
+#     queryset = Extrashift.objects.all()
+#     serializer_class = ExtrashiftSerializers
+
+# #its for Updateing Object View After the object got some Change
+# class ExtrashiftUpdateView(UpdateAPIView):
+#     queryset = Extrashift.objects.all()
+#     serializer_class = ExtrashiftSerializers
+
+# #its for Deleting Object View
+# class ExtrashiftDeleteView(DestroyAPIView):
+#     queryset = Extrashift.objects.all()
+#     serializer_class = ExtrashiftSerializers
