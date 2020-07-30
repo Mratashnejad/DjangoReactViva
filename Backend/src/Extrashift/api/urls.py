393c1e7430dfe0fef413fedc04c9a4248aefd6ae
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-07-30 09:17:47 +0400

    CreateView added Sucessfully

--- Backend/src/Extrashift/api/urls.py
+++ Backend/src/Extrashift/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views import ExtrashiftListView, ExtrashiftDetailView
+from .views import (
+    ExtrashiftListView,
+    ExtrashiftDetailView,
+    ExtrashiftCreateView
+)
 
 
 urlpatterns = [
     path('', ExtrashiftListView.as_view()),
+    path('create/', ExtrashiftCreateView.as_view()),
     path('<pk>', ExtrashiftDetailView.as_view()),
 ]
--- Backend/src/Extrashift/api/views.py
+++ Backend/src/Extrashift/api/views.py
@@ -1,13 +1,23 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 from Extrashift.models import Extrashift
 from .Serializers import ExtrashiftSerializers
 
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView
+)
 
+#its for List API Object View
 class ExtrashiftListView(ListAPIView):
     queryset = Extrashift.objects.all()
     serializer_class = ExtrashiftSerializers
 
-
+#its for Detail of API Object View
 class ExtrashiftDetailView(RetrieveAPIView):
+    queryset = Extrashift.objects.all()
+    serializer_class = ExtrashiftSerializers
+
+#its for Create New Object View
+class ExtrashiftCreateView(CreateAPIView):
     queryset = Extrashift.objects.all()
     serializer_class = ExtrashiftSerializers
