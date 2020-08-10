cf89248705d1085422f8a59e0b2cc928f0d27b30
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-08-10 12:31:16 +0400

    change all form input methods

--- Frontend/Ui/src/components/ExtrashiftForm.js
+++ Frontend/Ui/src/components/ExtrashiftForm.js
@@ -1,36 +1,28 @@
 import React from "react";
-import axios from "axios";
+import { Form, Input, Button } from "antd";
 import { connect } from "react-redux";
-import {
-  Form,
-  Input,
-  Button,
-  Select,
-  Switch,
-  DatePicker,
-  InputNumber,
-} from "antd";
+import axios from "axios";
 
 const FormItem = Form.Item;
-const { Option } = Select;
-// const onFinish = values => {
 
-//   console.log(values);
-// };
 
 class ExtrashiftForm extends React.Component {
+  
   handleFormSubmit = async (event, requestType, ExtrashiftID) => {
     event.preventDefault();
+
     const postObj = {
       title: event.target.elements.title.value,
       manager: event.target.elements.manager.value
     }
+
     axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
     axios.defaults.xsrfCookieName = "csrftoken";
     axios.defaults.headers = {
       "Content-Type": "application/json",
       Authorization: `Token ${this.props.token}`,
     };
+    
     if (requestType === "post") {
       await axios.post("http://127.0.0.1:8000/api/create/", postObj)
         .then(res => {
@@ -39,107 +31,36 @@ class ExtrashiftForm extends React.Component {
           }
         })
     } else if (requestType === "put") {
-      await axios.put(`http://127.0.0.1:8000/api/${articleID}/update/`, postObj)
+      await axios.put(`http://127.0.0.1:8000/api/${ExtrashiftID}/update/`, postObj)
         .then(res => {
           if (res.status === 200) {
             this.props.history.push(`/`);
           }
         })
-
-    // switch (requestType) {
-    //   case "post":
-    //     return axios
-    //       .post("url://127.0.0.1:8000/api/", {
-    //         title: title,
-    //         manager: manager,
-    //         datetime: datetime,
-    //         gender: gender,
-    //         Lable: Lable,
-    //         quantity: quantity,
-    //       })
-    //       .then((res) => console.log(res))
-    //       .catch((err) => console.err(err));
-    //   case "put":
-    //     return axios
-    //       .put(`http://127.0.0.1:8000/api/${ExtrashiftID}/`, {
-    //         title: title,
-    //         manager: manager,
-    //         datetime: datetime,
-    //         gender: gender,
-    //         Lable: Lable,
-    //         quantity: quantity,
-    //       })
-    //       .then((res) => console.log(res))
-    //       .catch((err) => console.err(err));
-    // }
-
-    return axios
-      .post("url://127.0.0.1:8000/api/", {
-        title: title,
-        manager: manager,
-        datetime: datetime,
-        gender: gender,
-        Lable: Lable,
-        quantity: quantity,
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
+    }
   };
+
   render() {
     return (
       <div>
         <Form
-          onFinish={(values) =>
-            this.handleFormSubmit(values, this.props.values)
+          onSubmit={event =>
+            this.handleFormSubmit(
+              event,
+              this.props.requestType,
+              this.props.ExtrashiftID
+            )
           }
         >
-          <FormItem name="title" label="Title">
-            <Input placeholder="title here" />
+          <FormItem label="Title">
+            <Input name="title" placeholder="Put a title here" />
           </FormItem>
-          <Form.Item
-            name="manager"
-            label="Manager"
-            hasFeedback
-            rules={[{ required: true, message: "Please select manager!" }]}
-          >
-            <Select placeholder="Please select a manager">
-              <Option value="Sa">Sargis</Option>
-              <Option value="La">Lara</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name="gender"
-            label="Gender"
-            rules={[
-              {
-                required: true,
-                message: "Please select Gender !",
-                type: "array",
-              },
-            ]}
-          >
-            <Select mode="multiple" placeholder="Please select Gender !">
-              <Option value="red">Female</Option>
-              <Option value="green">Male</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item name="Lable" label="Lable" valuePropName="checked">
-            <Switch />
-          </Form.Item>
-          <Form.Item label="quantity">
-            <Form.Item name="quantity" noStyle>
-              <InputNumber min={1} max={10} />
-            </Form.Item>
-            <span className="ant-form-text">needs</span>
-          </Form.Item>
-          <Form.Item name="date-picker" label="DatePicker">
-            <DatePicker />
-          </Form.Item>
+          <FormItem label="Content">
+            <Input name="manager" placeholder="Enter manager name" />
+          </FormItem>
           <FormItem>
             <Button type="primary" htmlType="submit">
-              Ok
+              {this.props.btnText}
             </Button>
           </FormItem>
         </Form>
@@ -147,4 +68,11 @@ class ExtrashiftForm extends React.Component {
     );
   }
 }
-export default ExtrashiftForm;
+
+const mapStateToProps = state => {
+  return {
+    token: state.token
+  };
+};
+
+export default connect(mapStateToProps)(ExtrashiftForm);
